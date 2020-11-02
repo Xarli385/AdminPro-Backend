68201427285a632acc05835882bde6c55b191dc3
= <=>
2020-11-02 17:25:43 +0100

    Fin BackEnd

--- controllers/medicos.js
+++ controllers/medicos.js
@@ -40,18 +40,88 @@ const crearMedico = async (req, res = response) => {
 
 }
 
-const actualizarMedico = (req, res = response) => {
-    res.json({
-        ok: true,
-        msg: 'actualizarMedico'
-    })
+const actualizarMedico = async (req, res = response) => {
+    try {
+
+        const id = req.params.id;
+        const uid = req.uid;
+
+        //const hospitalId = req.params.hospital;
+
+        const medico = await Medico.findById( id );
+        //const hospital = await Hospital.findById( hospitalId );
+
+        if (!medico){
+            return res.status(404).json({
+                ok: false,
+                msg: 'Medico no encontrado por Id'
+            })
+
+        }/* else if (!hospital){
+            return res.status(404).json({
+                ok: false,
+                msg: 'Hospital no encontrado'
+            })
+
+        }
+ */
+
+
+        //medico.id = req.body.id;
+
+
+        const cambiosMedico = {
+            ...req.body,
+            usuario: uid
+        }
+
+        const medicoActualizado = await Medico.findByIdAndUpdate ( id, cambiosMedico, { new: true });
+
+        res.json({
+            ok: true,
+            medico: medicoActualizado
+        })
+        
+    } catch (error) {
+
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        })
+        
+    }
 }
 
-const borrarMedico = (req, res = response) => {
-    res.json({
-        ok: true,
-        msg: 'borrarMedico'
-    })
+const borrarMedico = async (req, res = response) => {
+    try {
+
+        const id = req.params.id;
+
+        const medico = await Medico.findById( id );
+
+        if (!medico){
+            return res.status(404).json({
+                ok: false,
+                msg: 'Medico no encontrado por Id'
+            })
+
+        }    
+
+        await Medico.findByIdAndDelete ( id );
+
+        res.json({
+            ok: true,
+            msg: 'Médico Eliminado'
+        })
+        
+    } catch (error) {
+
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        })
+        
+    }
 }
 
 module.exports = {
